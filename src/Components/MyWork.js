e3123bf5ff9c23db15553dad2c6513cf150473e0
Miguel Roa <miguel_roa150@outlook.es>
2023-01-16 17:55:25 -0500

    informacion

--- src/Components/MyWork.js
+++ src/Components/MyWork.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const MyWork = () => {
     return (
-        <h1>
-        MyWork
-        </h1>
+        <div>
+            <h1>
+            MyWork
+            </h1>
+            <h2>
+                Experience: CONTACTO SOLUTIONS SAS ( Jul 2021  - Jan 2022 ) <br></br> <p>2021:  Backend and tecnical documentation.</p>
+            </h2>
+
+        </div>
     )
 }
 export default MyWork
