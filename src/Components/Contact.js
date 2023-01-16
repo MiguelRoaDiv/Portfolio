2e9c9b9164f138de3aab5925a39b07c4c22e2587
Miguel Roa <miguel_roa150@outlook.es>
2023-01-16 17:49:46 -0500

    correccion de iconos

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import linkedin from '../img/linkedin.png'
+import insta from '../img/insta.png'
+import github2 from '../img/github.png'
 
 const Contact = () => {
     return (
+        <div>
         <h1>
         Contact
         </h1>
+            <img src={linkedin} className="linkedin" alt={linkedin}/>
+            <img src={insta} className="insta" alt={insta}/>
+            <img src={github2} className="github2" alt={github2}/>
+        </div>
     )
 }
 export default Contact
